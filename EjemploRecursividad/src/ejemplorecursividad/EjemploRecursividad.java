24d8221237cf487fbf2bd1b8d7cd83b794e37012
dwn84 <dwn84@hotmail.com>
2020-03-21 09:55:05 -0500

    Ejemplos recursividad

---
+++ EjemploRecursividad/src/ejemplorecursividad/EjemploRecursividad.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplorecursividad;
+
+/**
+ *
+ * @author samaniw
+ */
+public class EjemploRecursividad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Recursividad re = new Recursividad();
+        System.out.println("Sumuatoria de 5");
+        System.out.println(re.Sumatoria(5));
+        System.out.println("Factorial de 5");
+        System.out.println(re.Factorial(5));
+        System.out.println("n-avo dato de Fibonacci");
+        System.out.println(re.Fibo1(3));
+        System.out.println("Serie Fibonacci método 1");
+        re.Fibo2(10);
+        System.out.println("");
+        System.out.println("Serie Fibonacci método 2");
+        re.FibR(10);
+    }
+    
+  
+    
+}
---
+++ EjemploRecursividad/src/ejemplorecursividad/Recursividad.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplorecursividad;
+
+/**
+ *
+ * @author samaniw
+ */
+public class Recursividad {
+    
+    private int a,b,c;
+    
+    public Recursividad(){
+        a=0;
+        b=1;
+    }
+
+    
+    public int Sumatoria(int n) {
+        //caso base
+        if (n == 1) {
+            return 1;
+        } else {
+            return Sumatoria(n - 1) + n;
+        }
+    }
+
+    public double Factorial(double n) {
+        //caso base
+        if (n == 0) {
+            return 1;
+        } else {
+            return Factorial(n - 1) * n;
+        }
+    }
+
+    public int Fibo1(int n) {
+        //caso base
+        /*
+      if(n==0){
+          return 0;
+      }else if(n==1){
+          return 1;
+      }
+         */
+        if (n < 2) {
+            return n;
+        } else {
+            return Fibo1(n - 1) + Fibo1(n - 2);
+        }
+    }
+    
+    public void Fibo2(int n){
+        //condicion de finalizacion
+        if(n>=1){
+            c=a+b;
+            System.out.print(a+" ");
+            a=b;
+            b=c;
+            Fibo2(n-1);
+        }            
+    }
+    
+    public void FibR(int n){
+        Fib3(n,0,1);
+    }
+    
+    private void Fib3(int n, int aa, int bb){
+        //caso baso
+        if(n==0){
+            return;
+        }else{
+            System.out.print(aa+" ");
+            Fib3(n-1, bb, aa+bb);
+        }
+    }
+    
+}
